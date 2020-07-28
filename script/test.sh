6f258d5ba5b3fd373790e0f9de72f6be4fb390c7
Certseeds <51754303+Certseeds@users.noreply.github.com>
2020-07-28 14:23:52 +0800

    update test action.

--- script/test.sh
+++ script/test.sh
@@ -6,12 +6,10 @@ set -eoux pipefail
 # @Author: nanoseeds
 # @Date: 2020-07-27 21:48:41
  # @LastEditors: nanoseeds
- # @LastEditTime: 2020-07-27 22:33:04
+ # @LastEditTime: 2020-07-28 14:23:01
 ###
 main() {
     old_env=$(pwd)
-    echo "${old_env}"
-    cd ./../cmake_build_path
     prefix="basic_algorithms"
     ./CS203_DSAA_template_test
     cd ./algorithm
@@ -21,11 +19,29 @@ main() {
         cd ..
     }
     {
-        cd ./binary_searchs
+        cd ./binary_search
         ./${prefix}_binary_search
         cd ..
     }
-    cd "${old_env}"
+    cd ..
+    # now at ./../cmake_build_path
 }
-
+try_mkdir() {
+    cd ./..
+    if [ ! -d "cmake_build_path" ]; then
+        mkdir ./cmake_build_path
+    fi
+    cd ./cmake_build_path
+    cmake ..
+    make
+    # now at ./../cmake_build_path
+}
+clear(){
+    cd ..
+    rm -rf "cmake_build_path"
+    # now at ./../
+}
+# now in ./
+try_mkdir
 main
+clear
