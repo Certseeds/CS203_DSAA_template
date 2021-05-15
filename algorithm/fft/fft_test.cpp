71b35852e6fda2b7ece6d4babc4158978470d255
Certseeds <51754303+Certseeds@users.noreply.github.com>
2021-05-15 10:38:55 +0800

    update fft
    </subject>
    
    Branch: master
    
    <type>:
    - [ ] Bug fix
    - [ ] Bug fix (Test)
    - [x] New feature
    - [ ] Breaking change
    - [ ] Documentation update
    - [ ] This change requires a documentation update
    
    <body>
    
    <footer>
    
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

--- algorithm/fft/fft_test.cpp
+++ algorithm/fft/fft_test.cpp
@@ -35,85 +35,85 @@ SOFTWARE.
 #include <catch_main.hpp>
 
 namespace fft {
-    using Catch::Matchers::Equals;
-    using Catch::Matchers::UnorderedEquals;
-    using Catch::Matchers::Contains;
+using Catch::Matchers::Equals;
+using Catch::Matchers::UnorderedEquals;
+using Catch::Matchers::Contains;
 
-    TEST_CASE("basic -2", "[algorithm fft]") {
-        const vector<int32_t> vec1{num_to_vec(21ull)};
-        const vector<int32_t> vec2{num_to_vec(43ull)};
-        const vector<int32_t> result{num_to_vec(903ull)};
-        CHECK_THAT(multiply(vec1, vec2), Equals(result));
-    }
+TEST_CASE("basic -2", "[algorithm fft]") {
+    const vector<int32_t> vec1{num_to_vec(21ull)};
+    const vector<int32_t> vec2{num_to_vec(43ull)};
+    const vector<int32_t> result{num_to_vec(903ull)};
+    CHECK_THAT(multiply(vec1, vec2), Equals(result));
+}
 
-    TEST_CASE("basic -1", "[algorithm fft]") {
-        const vector<int32_t> vec1{num_to_vec(4211ull)};
-        const vector<int32_t> vec2{num_to_vec(3655ull)};
-        const vector<int32_t> result{num_to_vec(15391205ull)};
-        CHECK_THAT(multiply(vec1, vec2), Equals(result));
-    }
+TEST_CASE("basic -1", "[algorithm fft]") {
+    const vector<int32_t> vec1{num_to_vec(4211ull)};
+    const vector<int32_t> vec2{num_to_vec(3655ull)};
+    const vector<int32_t> result{num_to_vec(15391205ull)};
+    CHECK_THAT(multiply(vec1, vec2), Equals(result));
+}
 
-    TEST_CASE("basic 0", "[algorithm fft]") {
-        const vector<int32_t> vec1{num_to_vec(21ull)};
-        const vector<int32_t> vec2{num_to_vec(35ull)};
-        const vector<int32_t> result{num_to_vec(735ull)};
-        CHECK_THAT(multiply(vec1, vec2), Equals(result));
-    }
+TEST_CASE("basic 0", "[algorithm fft]") {
+    const vector<int32_t> vec1{num_to_vec(21ull)};
+    const vector<int32_t> vec2{num_to_vec(35ull)};
+    const vector<int32_t> result{num_to_vec(735ull)};
+    CHECK_THAT(multiply(vec1, vec2), Equals(result));
+}
 
-    TEST_CASE("basic 1", "[algorithm fft]") {
-        const vector<int32_t> vec1{num_to_vec(123ull)};
-        const vector<int32_t> vec2{num_to_vec(456ull)};
-        const vector<int32_t> result{num_to_vec(56088ull)};
-        CHECK_THAT(multiply(vec1, vec2), Equals(result));
-    }
+TEST_CASE("basic 1", "[algorithm fft]") {
+    const vector<int32_t> vec1{num_to_vec(123ull)};
+    const vector<int32_t> vec2{num_to_vec(456ull)};
+    const vector<int32_t> result{num_to_vec(56088ull)};
+    CHECK_THAT(multiply(vec1, vec2), Equals(result));
+}
 
-    TEST_CASE("basic 2", "[algorithm fft]") {
-        const vector<int32_t> vec1{num_to_vec(114514ull)};
-        const vector<int32_t> vec2{num_to_vec(1919810ull)};
-        const vector<int32_t> result{num_to_vec(219845122340ull)};
-        CHECK_THAT(multiply(vec1, vec2), Equals(result));
-    }
+TEST_CASE("basic 2", "[algorithm fft]") {
+    const vector<int32_t> vec1{num_to_vec(114514ull)};
+    const vector<int32_t> vec2{num_to_vec(1919810ull)};
+    const vector<int32_t> result{num_to_vec(219845122340ull)};
+    CHECK_THAT(multiply(vec1, vec2), Equals(result));
+}
 
-    TEST_CASE("basic 3", "[algorithm fft]") {
-        const vector<int32_t> vec1{num_to_vec(201037ull)};
-        const vector<int32_t> vec2{num_to_vec(20210513ull)};
-        const vector<int32_t> result{num_to_vec(4063060901981ull)};
-        CHECK_THAT(multiply(vec1, vec2), Equals(result));
-    }
+TEST_CASE("basic 3", "[algorithm fft]") {
+    const vector<int32_t> vec1{num_to_vec(201037ull)};
+    const vector<int32_t> vec2{num_to_vec(20210513ull)};
+    const vector<int32_t> result{num_to_vec(4063060901981ull)};
+    CHECK_THAT(multiply(vec1, vec2), Equals(result));
+}
 
-    TEST_CASE("extend check 0", "[algorithm fft]") {
-        vector<int32_t> vec1{1};
-        extend(vec1);
-        CHECK_THAT(vec1, Equals(vector<int32_t>{1}));
-    }
+TEST_CASE("extend check 0", "[algorithm fft]") {
+    vector<int32_t> vec1{1};
+    extend(vec1);
+    CHECK_THAT(vec1, Equals(vector<int32_t>{1}));
+}
 
-    TEST_CASE("extend check 1", "[algorithm fft]") {
-        vector<int32_t> vec1{1, 2};
-        extend(vec1);
-        CHECK_THAT(vec1, Equals(vector<int32_t>{1, 2}));
-    }
+TEST_CASE("extend check 1", "[algorithm fft]") {
+    vector<int32_t> vec1{1, 2};
+    extend(vec1);
+    CHECK_THAT(vec1, Equals(vector<int32_t>{1, 2}));
+}
 
-    TEST_CASE("extend check 2", "[algorithm fft]") {
-        vector<int32_t> vec1{};
-        extend(vec1);
-        CHECK_THAT(vec1, Equals(vector<int32_t>{}));
-    }
+TEST_CASE("extend check 2", "[algorithm fft]") {
+    vector<int32_t> vec1{};
+    extend(vec1);
+    CHECK_THAT(vec1, Equals(vector<int32_t>{}));
+}
 
-    TEST_CASE("extend check 3", "[algorithm fft]") {
-        vector<int32_t> vec1{1, 2, 3};
-        extend(vec1);
-        CHECK_THAT(vec1, Equals(vector<int32_t>{1, 2, 3, 0}));
-    }
+TEST_CASE("extend check 3", "[algorithm fft]") {
+    vector<int32_t> vec1{1, 2, 3};
+    extend(vec1);
+    CHECK_THAT(vec1, Equals(vector<int32_t>{1, 2, 3, 0}));
+}
 
-    TEST_CASE("extend check 4", "[algorithm fft]") {
-        vector<int32_t> vec1{1, 2, 3, 4};
-        extend(vec1);
-        CHECK_THAT(vec1, Equals(vector<int32_t>{1, 2, 3, 4}));
-    }
+TEST_CASE("extend check 4", "[algorithm fft]") {
+    vector<int32_t> vec1{1, 2, 3, 4};
+    extend(vec1);
+    CHECK_THAT(vec1, Equals(vector<int32_t>{1, 2, 3, 4}));
+}
 
-    TEST_CASE("extend check 5", "[algorithm fft]") {
-        vector<int32_t> vec1{1,2,3,4,5};
-        extend(vec1);
-        CHECK_THAT(vec1, Equals(vector<int32_t>{1,2,3,4,5,0,0,0}));
-    }
+TEST_CASE("extend check 5", "[algorithm fft]") {
+    vector<int32_t> vec1{1, 2, 3, 4, 5};
+    extend(vec1);
+    CHECK_THAT(vec1, Equals(vector<int32_t>{1, 2, 3, 4, 5, 0, 0, 0}));
+}
 }
--- algorithm/fft/unfft.cpp
+++ algorithm/fft/unfft.cpp
@@ -34,18 +34,18 @@ SOFTWARE.
 #include "fft.hpp"
 
 namespace fft {
-    vector<int32_t> multiply(const vector<int32_t> &one, const vector<int32_t> &sec) {
-        size_t length = std::max(one.size(), sec.size());
-        vector<int32_t> will_return(length << 1, 0);
-        for (size_t i{0}; i < one.size(); ++i) {
-            for (size_t j{0}; j < sec.size(); ++j) {
-                will_return[i + j] += one[i] * sec[j];
-            }
+vector<int32_t> multiply(const vector<int32_t> &one, const vector<int32_t> &sec) {
+    size_t length = std::max(one.size(), sec.size());
+    vector<int32_t> will_return(length << 1, 0);
+    for (size_t i{0}; i < one.size(); ++i) {
+        for (size_t j{0}; j < sec.size(); ++j) {
+            will_return[i + j] += one[i] * sec[j];
         }
-        for (size_t i{1}; i < will_return.size(); ++i) {
-            will_return[i] += will_return[i - 1] / base;
-            will_return[i - 1] %= base;
-        }
-        return will_return;
     }
+    for (size_t i{1}; i < will_return.size(); ++i) {
+        will_return[i] += will_return[i - 1] / base;
+        will_return[i - 1] %= base;
+    }
+    return will_return;
+}
 }
