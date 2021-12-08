4469f441ce567465fb4f0ce306d4239db5455a6b
Certseeds <51754303+Certseeds@users.noreply.github.com>
2021-12-08 23:11:03 +0800

    feature: update leetcode-42
    </subject>
    
    Branch: dev
    
    <type>:
    - [ ] Bug fix
    - [ ] Bug fix (Test)
    - [x] New feature
    - [ ] Breaking change
    - [ ] Documentation update
    - [ ] This change requires a documentation update
    
    <body>
    
    <footer>
    
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

--- algorithm/stack/leetcode_42_test.hpp
+++ algorithm/stack/leetcode_42_test.hpp
@@ -26,6 +26,8 @@ SOFTWARE.
 //@Tag stack
 //@Tag 栈
 //@Tag 单调栈
+//@Tag DP
+//@Tag 动态规划
 #ifndef CS203_DSAA_TEMPLATE_ALGORITHM_STACK_LEETCODE_42_HPP
 #define CS203_DSAA_TEMPLATE_ALGORITHM_STACK_LEETCODE_42_HPP
 
@@ -36,23 +38,35 @@ namespace leetcode_42 {
 using std::stack;
 
 struct leetcode_42 {
-    static int trap(const vector<int> &height);
+    static int32_t trap(const vector<int32_t> &height);
+
+    static int32_t trapDPLoca(const vector<int32_t> &height);
+
+    static int32_t trapDPHeight(const vector<int32_t> &height);
 };
-
-
-TEST_CASE("test case 1 [test _42]", "[test _42]") {
-    const vector<int32_t> input{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
-    CHECK(6 == leetcode_42::trap(input));
-}
 
 TEST_CASE("test case 2 [test _42]", "[test _42]") {
     const vector<int32_t> input{4, 2, 0, 3, 2, 5};
-    CHECK(9 == leetcode_42::trap(input));
+    static constexpr const auto result{9};
+    CHECK(result == leetcode_42::trap(input));
+    CHECK(result == leetcode_42::trapDPLoca(input));
+    CHECK(result == leetcode_42::trapDPHeight(input));
+}
+
+TEST_CASE("test case 1 [test _42]", "[test _42]") {
+    const vector<int32_t> input{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+    static constexpr const auto result{6};
+    CHECK(result == leetcode_42::trap(input));
+    CHECK(result == leetcode_42::trapDPLoca(input));
+    CHECK(result == leetcode_42::trapDPHeight(input));
 }
 
 TEST_CASE("test case 3 [test _42]", "[test _42]") {
     const vector<int32_t> input{0, 1, 0, 3, 1, 0, 1, 3, 2, 1, 2, 1};
-    CHECK(9 == leetcode_42::trap(input));
+    static constexpr const auto result{9};
+    CHECK(result == leetcode_42::trap(input));
+    CHECK(result == leetcode_42::trapDPLoca(input));
+    CHECK(result == leetcode_42::trapDPHeight(input));
 }
 }
 #endif //CS203_DSAA_TEMPLATE_ALGORITHM_STACK_LEETCODE_42_HPP
