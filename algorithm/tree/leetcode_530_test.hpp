1b01c7da0be5136135282c69cdb7205fe8dc252e
Certseeds <51754303+Certseeds@users.noreply.github.com>
2021-12-31 00:09:37 +0800

    feature: add leetcode-500,530
    </subject>
    
    Branch: dev
    
    <type>:
    - [ ] Bug fix
    - [ ] Bug fix (Test)
    - [x] New feature
    - [ ] Breaking change
    - [ ] Documentation update
    - [ ] This change requires a documentation update
    
    <body>
    
    <footer>
    
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

---
+++ algorithm/tree/leetcode_530_test.hpp
@@ -0,0 +1,75 @@
+/*
+MIT License
+
+CS203_DSAA_template
+
+Copyright (C) 2020-2021  nanoseeds
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+//@Tag tree
+//@Tag 树
+//@Tag in-traverse
+//@Tag 中序遍历二叉树
+#ifndef CS203_DSAA_TEMPLATE_ALGORITHM_TREE_LEETCODE_530_HPP
+#define CS203_DSAA_TEMPLATE_ALGORITHM_TREE_LEETCODE_530_HPP
+
+#include <catch_main.hpp>
+#include <tree/TreeNode.hpp>
+#include <tree/TreeNodeLink.hpp>
+#include <stack>
+
+namespace leetcode_530 {
+
+using TreeNode = TREE_NODE::TreeNode<int32_t>;
+using TreeNodeLink = TREE_NODE::TreeNodeLink<int32_t>;
+using TREE_NODE::numToTree;
+using std::stack;
+
+struct leetcode_530 {
+    static int32_t getMinimumDifference(TreeNode *root);
+
+    static int32_t getMinimumDifference2(TreeNode *root);
+};
+
+TEST_CASE("test_case 1 [test_530]", "[test_530]") {
+    const vector<int32_t> input{4, 2, 6, 1, 3};
+    vector<TreeNode *> numVecInput = numToTree<int32_t>(input);
+    static constexpr const auto result{1};
+    THEN("case 1") {
+        CHECK(result == leetcode_530::getMinimumDifference(numVecInput[0]));
+    }THEN("case 2") {
+        CHECK(result == leetcode_530::getMinimumDifference2(numVecInput[0]));
+    }
+    const TreeNodeLink link{numVecInput.front()};
+}
+
+TEST_CASE("test_case 2 [test_530]", "[test_530]") {
+    const vector<int32_t> input{1, 0, 48, TreeNode::No, TreeNode::No, 12, 49};
+    vector<TreeNode *> numVecInput = numToTree<int32_t>(input);
+    static constexpr const auto result{1};
+    THEN("case 1") {
+        CHECK(result == leetcode_530::getMinimumDifference(numVecInput[0]));
+    }THEN("case 2") {
+        CHECK(result == leetcode_530::getMinimumDifference2(numVecInput[0]));
+    }
+    const TreeNodeLink link{numVecInput.front()};
+}
+}
+#endif //CS203_DSAA_TEMPLATE_ALGORITHM_TREE_LEETCODE_530_HPP
