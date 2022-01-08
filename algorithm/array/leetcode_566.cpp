f7eb2568d93d4c949f4e35cfd73ccab9af0ed49b
Certseeds <51754303+Certseeds@users.noreply.github.com>
2022-01-08 14:04:29 +0800

    feature: add leetcode-566,581
    </subject>
    
    Branch: dev
    
    <type>:
    - [ ] Bug fix
    - [ ] Bug fix (Test)
    - [x] New feature
    - [ ] Breaking change
    - [ ] Documentation update
    - [ ] This change requires a documentation update
    
    <body>
    
    <footer>
    
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

---
+++ algorithm/array/leetcode_566.cpp
@@ -0,0 +1,45 @@
+/*
+MIT License
+
+CS203_DSAA_template
+
+Copyright (C) 2020-2021  nanoseeds
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+#include "leetcode_566_test.hpp"
+
+namespace leetcode_566 {
+
+vector<vector<int32_t>> leetcode_566::matrixReshape(const vector<vector<int32_t>> &mat, int32_t r, int32_t c) {
+    const auto lev1{mat.size()}, lev2{mat.front().size()};
+    if (r * c - lev1 * lev2 != 0) {
+        return vector<vector<int32_t>>{mat};
+    }
+    vector<vector<int32_t>> willreturn(r, vector<int32_t>(c));
+    for (int32_t i{0}, sum{0}; i < r; i++) {
+        for (int32_t j{0}; j < c; j++, sum++) {
+            willreturn[i][j] = mat[sum / lev2][sum % lev2];
+        }
+    }
+    return willreturn;
+}
+
+
+}
---
+++ algorithm/array/leetcode_581_test.hpp
@@ -0,0 +1,73 @@
+/*
+MIT License
+
+CS203_DSAA_template
+
+Copyright (C) 2020-2021  nanoseeds
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+//@Tag array
+//@Tag 数组
+#ifndef CS203_DSAA_TEMPLATE_ALGORITHM_ARRAY_LEETCODE_581_HPP
+#define CS203_DSAA_TEMPLATE_ALGORITHM_ARRAY_LEETCODE_581_HPP
+
+#include <catch_main.hpp>
+#include <algorithm>
+#include <cstdint>
+#include <cstddef>
+#include <vector>
+
+namespace leetcode_581 {
+using std::vector;
+
+struct leetcode_581 {
+    static int32_t findUnsortedSubarray(const vector<int32_t> &nums);
+    static int32_t findUnsortedSubarray2(const vector<int32_t> &nums);
+};
+
+TEST_CASE("test case 1 [test_581]", "[test_581]") {
+    const vector<int32_t> input{1, 1, 4, 5, 1, 4};
+    static constexpr const auto result{4};
+    CHECK(result == leetcode_581::findUnsortedSubarray(input));
+    CHECK(result == leetcode_581::findUnsortedSubarray2(input));
+}
+
+TEST_CASE("test case 2 [test_581]", "[test_581]") {
+    const vector<int32_t> input{2, 6, 4, 8, 10, 9, 15};
+    static constexpr const auto result{5};
+    CHECK(result == leetcode_581::findUnsortedSubarray(input));
+    CHECK(result == leetcode_581::findUnsortedSubarray2(input));
+}
+
+TEST_CASE("test case 3 [test_581]", "[test_581]") {
+    const vector<int32_t> input{1, 2, 3, 4};
+    static constexpr const auto result{0};
+    CHECK(result == leetcode_581::findUnsortedSubarray(input));
+    CHECK(result == leetcode_581::findUnsortedSubarray2(input));
+}
+
+TEST_CASE("test case 4 [test_581]", "[test_581]") {
+    const vector<int32_t> input{1};
+    static constexpr const auto result{0};
+    CHECK(result == leetcode_581::findUnsortedSubarray(input));
+    CHECK(result == leetcode_581::findUnsortedSubarray2(input));
+}
+}
+#endif //CS203_DSAA_TEMPLATE_ALGORITHM_ARRAY_LEETCODE_581_HPP
