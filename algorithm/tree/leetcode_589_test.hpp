421d54afc938a323f6f63d1886d466ceab162634
Certseeds <51754303+Certseeds@users.noreply.github.com>
2022-01-10 00:22:45 +0800

    feature: finish leetcode-589
    </subject>
    
    Branch: dev
    
    <type>:
    - [ ] Bug fix
    - [ ] Bug fix (Test)
    - [x] New feature
    - [ ] Breaking change
    - [ ] Documentation update
    - [ ] This change requires a documentation update
    
    <body>
    
    <footer>
    
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

---
+++ algorithm/tree/leetcode_589_test.hpp
@@ -0,0 +1,50 @@
+/*
+MIT License
+
+CS203_DSAA_template
+
+Copyright (C) 2020-2021  nanoseeds
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+//@Tag tree
+//@Tag 树
+#ifndef CS203_DSAA_TEMPLATE_ALGORITHM_TREE_LEETCODE_589_HPP
+#define CS203_DSAA_TEMPLATE_ALGORITHM_TREE_LEETCODE_589_HPP
+
+#include <catch_main.hpp>
+#include <tree/treenode_multi.hpp>
+#include <stack>
+
+namespace leetcode_589 {
+
+using Node = TREE_NODE::TreeNodeMulti;
+using std::stack;
+
+struct leetcode_589 final {
+    static vector<int32_t> preorder(const Node *root);
+
+    static vector<int32_t> preorderIter(const Node *root);
+};
+
+TEST_CASE("test_case 1 [test_589]", "[test_589]") {
+    // TODO
+}
+}
+#endif //CS203_DSAA_TEMPLATE_ALGORITHM_TREE_LEETCODE_589_HPP
