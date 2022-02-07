3b9ac46a1169049134e3a7c050ffbf7e5830d36d
Certseeds <51754303+Certseeds@users.noreply.github.com>
2022-02-07 11:13:41 +0800

    feature: add sword_offer 09,30
    </subject>
    
    Branch: dev
    
    <type>:
    - [ ] Bug fix
    - [ ] Bug fix (Test)
    - [x] New feature
    - [ ] Breaking change
    - [ ] Documentation update
    - [ ] This change requires a documentation update
    
    <body>
    
    <footer>
    
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

---
+++ algorithm/stack/sword_offer_30.cpp
@@ -0,0 +1,104 @@
+/*
+MIT License
+
+CS203_DSAA_template
+
+Copyright (C) 2020-2021  nanoseeds
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+#include "sword_offer_30_test.hpp"
+
+namespace sword_offer_30 {
+struct MinStackPure : MinStack {
+private:
+    stack<int32_t> sta1, sta2;
+public:
+    MinStackPure() : MinStack() {};
+
+    void push(int x) override final {
+        sta1.push(x);
+        if (sta2.empty()) {
+            sta2.push(x);
+        } else {
+            const auto minV = std::min(x, sta2.top());
+            sta2.push(minV);
+        }
+    }
+
+    void pop() override final {
+        sta1.pop();
+        sta2.pop();
+    }
+
+    int top() override final {
+        return sta1.top();
+    }
+
+    int min() override final {
+        return sta2.top();
+    }
+};
+
+struct MinStackEffective : MinStack {
+private:
+    stack<int32_t> sta1, sta2;
+public:
+    MinStackEffective() : MinStack() {};
+
+    void push(int x) final {
+        if (sta2.empty()) {
+            sta2.push(x);
+        } else {
+            if (sta2.top() >= x) {
+                sta2.push(x);
+            }
+        }
+        sta1.push(x);
+    }
+
+    void pop() final {
+        if (sta1.empty()) {
+            return;
+        }
+        const auto top = this->top();
+        sta1.pop();
+        if (!sta2.empty() && top == this->min()) {
+            sta2.pop();
+        }
+    }
+
+    int top() final {
+        return sta1.top();
+    }
+
+    int min() final {
+        return sta2.top();
+    }
+};
+
+MinStack *sword_offer_30::pure() {
+    return new MinStackPure();
+}
+
+MinStack *sword_offer_30::effective() {
+    return new MinStackEffective();
+}
+
+}
---
+++ algorithm/stack/sword_offer_30_test.hpp
@@ -0,0 +1,93 @@
+/*
+MIT License
+
+CS203_DSAA_template
+
+Copyright (C) 2020-2021  nanoseeds
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+//@Tag stack
+//@Tag 栈
+#ifndef CS203_DSAA_TEMPLATE_ALGORITHM_QUEUE_SWORD_OFFER_30_HPP
+#define CS203_DSAA_TEMPLATE_ALGORITHM_QUEUE_SWORD_OFFER_30_HPP
+
+#include <catch_main.hpp>
+#include <stack>
+#include <string>
+
+namespace sword_offer_30 {
+using std::stack;
+using std::string;
+
+struct MinStack {
+    MinStack() = default;;
+
+    virtual void push(int x) = 0;
+
+    virtual void pop() = 0;
+
+    virtual int top() = 0;
+
+    virtual int min() = 0;
+
+    virtual ~MinStack() = default;
+};
+
+struct sword_offer_30 {
+    static MinStack *pure();
+
+    static MinStack *effective();
+};
+
+TEST_CASE("test case pure-1 [test_sw_30]", "[test_sw_30]") {
+    std::function<MinStack *()> lambda;
+    SECTION("pure") {
+        lambda = sword_offer_30::pure;
+    }SECTION("effective") {
+        lambda = sword_offer_30::effective;
+    }
+    const auto ptr = std::unique_ptr<MinStack>(lambda());
+    ptr->push(0);
+    ptr->push(1);
+    ptr->push(0);
+    CHECK(0 == ptr->min());
+    ptr->pop();
+    CHECK(0 == ptr->min());
+}
+
+TEST_CASE("test case pure-2 [test_sw_30]", "[test_sw_30]") {
+    std::function<MinStack *()> lambda;
+    SECTION("pure") {
+        lambda = sword_offer_30::pure;
+    }SECTION("effective") {
+        lambda = sword_offer_30::effective;
+    }
+    const auto ptr = std::unique_ptr<MinStack>(lambda());
+    ptr->push(-2);
+    ptr->push(0);
+    ptr->push(-1);
+    CHECK(-2 == ptr->min());
+    CHECK(-1 == ptr->top());
+    ptr->pop();
+    CHECK(-2 == ptr->min());
+    CHECK(0 == ptr->top());
+}
+}
+#endif //CS203_DSAA_TEMPLATE_ALGORITHM_QUEUE_SWORD_OFFER_30_HPP
