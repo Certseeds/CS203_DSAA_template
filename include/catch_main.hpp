ffd8fb6feccec0b12ddd9ea9de08657442b0be06
Certseeds <51754303+Certseeds@users.noreply.github.com>
2022-04-04 13:03:31 +0800

    feature: delete precompile headers
    </subject>
    
    Branch: dev
    
    <type>:
    - [ ] Bug fix
    - [ ] Bug fix (Test)
    - [x] New feature
    - [ ] Breaking change
    - [ ] Documentation update
    - [ ] This change requires a documentation update
    
    <body>
    
    <footer>
    
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

--- include/catch_main.hpp
+++ include/catch_main.hpp
@@ -7,21 +7,9 @@ Copyright (C) 2020-2022 nanoseeds
 */
 #ifndef CS203_DSAA_TEMPLATE_INCLUDES_CATCH_MAIN_H
 #define CS203_DSAA_TEMPLATE_INCLUDES_CATCH_MAIN_H
-//#define CATCH_CONFIG_MAIN
-
-#ifndef CATCH_AMALGAMATED_HPP_INCLUDED
 
 #include <catch_amalgamated.hpp>
-
-#pragma message("import catch_amalgamated.hpp")
-// find this in ide's autolight
-#else
-#pragma message("use pre-compiled")
-// output this while compile
-// 兼容MSVC
-#endif
-
-#include <public.hpp>
+#include <include/public.hpp>
 
 static const CS203_timer timer{};
 
