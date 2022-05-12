d23a35e8c16d9503acb296abdcaa67535af22782
Certseeds <51754303+Certseeds@users.noreply.github.com>
2022-05-12 14:55:10 +0000

    feature: add lc-872
    </subject>
    
    Branch: dev
    
    <type>:
    - [ ] Bug fix
    - [ ] Bug fix (Test)
    - [x] New feature
    - [ ] Breaking change
    - [ ] Documentation update
    - [ ] This change requires a documentation update
    
    <body>
    
    <footer>
    
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

---
+++ algorithm/tree/leetcode_872.cpp
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+/*
+CS203_DSAA_template
+
+Copyright (C) 2022 nanoseeds
+
+*/
+#include "leetcode_872_test.hpp"
+#include <stack>
+
+namespace leetcode_872 {
+vector<int32_t> getNode(TreeNode *root) {
+    vector<int32_t> willreturn{};
+    for (std::stack<TreeNode *> ques{{root}}; !ques.empty();) {
+        const TreeNode *rootnode = ques.top();
+        ques.pop();
+        if (rootnode->left != nullptr) {
+            ques.push(rootnode->left);
+        }
+        if (rootnode->right != nullptr) {
+            ques.push(rootnode->right);
+        }
+        if (rootnode->left == nullptr && rootnode->right == nullptr) {
+            willreturn.push_back(rootnode->val);
+        }
+    }
+    return willreturn;
+}
+
+bool leetcode_872::leafSimilar(TreeNode *root1, TreeNode *root2) {
+    if (root1 == nullptr && root2 == nullptr) {
+        return true;
+    } else if (root1 == nullptr || root2 == nullptr) {
+        return false;
+    }
+    vector<int32_t> val1 = getNode(root1);
+    vector<int32_t> val2 = getNode(root2);
+    if (val1.size() != val2.size()) {
+        return false;
+    }
+    for (size_t i{0}, max{val1.size()}; i < max; ++i) {
+        if (val1[i] != val2[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+}
---
+++ algorithm/tree/leetcode_872_test.hpp
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+/*
+CS203_DSAA_template
+
+Copyright (C) 2022  nanoseeds
+
+*/
+//@Tag tree
+//@Tag 树
+#ifndef CS203_DSAA_TEMPLATE_ALGORITHM_TREE_LEETCODE_872_TEST_HPP
+#define CS203_DSAA_TEMPLATE_ALGORITHM_TREE_LEETCODE_872_TEST_HPP
+
+#include <catch_main.hpp>
+#include <cstdint>
+#include <cstddef>
+#include <tree/treenode.hpp>
+#include <tree/treenode_link.hpp>
+#include <vector>
+
+namespace leetcode_872 {
+using std::vector;
+using TreeNode = TREE_NODE::TreeNode<int32_t>;
+
+struct leetcode_872 final {
+    static bool leafSimilar(TreeNode *root1, TreeNode *root2);
+};
+
+using TreeNodeLink = TREE_NODE::TreeNodeLink<int32_t>;
+using TREE_NODE::numToTree;
+
+TEST_CASE("test_case 1 [test_872]", "[test_872]") {
+    const TreeNodeLink input1{3,
+                              5, 1,
+                              6, 2, 9, 8,
+                              TreeNode::No, TreeNode::No, 7, 4
+    };
+    const TreeNodeLink input2{3,
+                              5, 1,
+                              6, 7, 4, 2,
+                              TreeNode::No, TreeNode::No, TreeNode::No, TreeNode::No, TreeNode::No, TreeNode::No, 9, 8
+    };
+    CHECK(leetcode_872::leafSimilar(input1[0], input2[0]));
+}
+
+TEST_CASE("test_case 2 [test_872]", "[test_872]") {
+    const TreeNodeLink input1{1, 2, 3};
+    const TreeNodeLink input2{1, 3, 2};
+    CHECK_FALSE(leetcode_872::leafSimilar(input1[0], input2[0]));
+}
+
+}
+#endif //CS203_DSAA_TEMPLATE_ALGORITHM_TREE_LEETCODE_872_TEST_HPP
