917a21997c069b73fba99ef6fad7d946ae425b62
Certseeds <51754303+Certseeds@users.noreply.github.com>
2022-05-28 14:10:50 +0000

    feature: update lc-1409
    </subject>
    
    Branch: develop
    
    <type>:
    - [ ] Bug fix
    - [ ] Bug fix (Test)
    - [x] New feature
    - [ ] Breaking change
    - [ ] Documentation update
    - [ ] This change requires a documentation update
    
    <body>
    
    添加树状数组
    
    PS: 对零值的处理值得关注
    
    <footer>
    
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

--- algorithm/associative_container/leetcode_1409.cpp
+++ algorithm/associative_container/leetcode_1409.cpp
@@ -8,6 +8,7 @@ Copyright (C) 2022  nanoseeds
 #include "leetcode_1409_test.hpp"
 #include <unordered_map>
 
+
 namespace leetcode_1409 {
 using std::unordered_map;
 
@@ -20,10 +21,10 @@ vector<int32_t> leetcode_1409::processQueries(const vector<int32_t> &queries, in
     for (int32_t i{0}; i < m; ++i) {
         num_pos[i + 1] = i;
     }
-    for (const auto &i:queries) {
+    for (const auto &i: queries) {
         int32_t pos = num_pos[i];
         will_return.push_back(pos);
-        for (auto &j:num_pos) {
+        for (auto &j: num_pos) {
             if (j.second < pos) {
                 j.second++;
             }
@@ -32,4 +33,49 @@ vector<int32_t> leetcode_1409::processQueries(const vector<int32_t> &queries, in
     }
     return will_return;
 }
+
+class bit final {
+private:
+    vector<int32_t> vec{};
+    const int32_t max_size;
+
+    static int32_t lowbit(int32_t num) {
+        return num & -num;
+    }
+
+public:
+    explicit bit(const int32_t size) : vec(size + 1, 0), max_size(size) {}
+
+    void update(const int32_t location, const int32_t num) {
+        assert(location >= 1); // 入参并不从零开始,
+        for (int32_t pos{location}; pos <= max_size; pos += lowbit(pos)) { vec[pos] += num; }
+    }
+
+    int32_t query(const int32_t location) const {
+        int32_t ans{0};
+        for (int32_t pos{location}; pos > 0; pos -= lowbit(pos)) { ans += vec[pos]; }
+        return ans;
+    }
+};
+
+vector<int32_t> leetcode_1409::processQueries2(const vector<int32_t> &queries, int32_t m) {
+    const int32_t query_nums = queries.size();
+    bit tree(m + query_nums);
+    vector<int32_t> pos(m + 1);
+    for (int i{1}; i <= m; ++i) {
+        pos[i] = query_nums + i;
+        tree.update(query_nums + i, 1);
+    }
+    vector<int32_t> ans;
+    for (int i{0}; i < query_nums; ++i) {
+        const auto query{queries[i]};
+        const auto loca = pos[query];
+        tree.update(loca, -1);
+        ans.push_back(tree.query(loca));
+        pos[query] = query_nums - i;
+        tree.update(pos[query], 1);
+    }
+    return ans;
+}
+
 }
--- algorithm/associative_container/leetcode_1409_test.hpp
+++ algorithm/associative_container/leetcode_1409_test.hpp
@@ -6,8 +6,7 @@ Copyright (C) 2022  nanoseeds
 
 */
 //@Tag associative_container
-//@Tag arrays as hashmap
-//@Tag 自定义hash
+
 #ifndef CS203_DSAA_TEMPLATE_ALGORITHM_ASSOCIATIVE_CONTAINER_LEETCODE_1409_TEST_HPP
 #define CS203_DSAA_TEMPLATE_ALGORITHM_ASSOCIATIVE_CONTAINER_LEETCODE_1409_TEST_HPP
 
@@ -22,8 +21,9 @@ using std::vector;
 
 struct leetcode_1409 {
     static vector<int32_t> processQueries(const vector<int32_t> &queries, int32_t m);
-};
 
+    static vector<int32_t> processQueries2(const vector<int32_t> &queries, int32_t m);
+};
 
 using Catch::Matchers::Equals;
 
@@ -32,8 +32,31 @@ TEST_CASE("test case 1 {test_1409}", "{test_1409}") {
     static constexpr const auto m{5};
     const vector<int32_t> result{2, 1, 2, 1};
     CHECK_THAT(result, Equals(leetcode_1409::processQueries(input, m)));
+    CHECK_THAT(result, Equals(leetcode_1409::processQueries2(input, m)));
 }
 
+TEST_CASE("test case 2 {test_1409}", "{test_1409}") {
+    const vector<int32_t> input{4, 1, 2, 2};
+    static constexpr const auto m{4};
+    const vector<int32_t> result{3, 1, 2, 0};
+    CHECK_THAT(result, Equals(leetcode_1409::processQueries(input, m)));
+    CHECK_THAT(result, Equals(leetcode_1409::processQueries2(input, m)));
+}
 
+TEST_CASE("test case 3 {test_1409}", "{test_1409}") {
+    const vector<int32_t> input{7, 5, 5, 8, 3};
+    static constexpr const auto m{8};
+    const vector<int32_t> result{6, 5, 0, 7, 5};
+    CHECK_THAT(result, Equals(leetcode_1409::processQueries(input, m)));
+    CHECK_THAT(result, Equals(leetcode_1409::processQueries2(input, m)));
+}
+
+TEST_CASE("test case 4 {test_1409}", "{test_1409}") {
+    const vector<int32_t> input{8, 7, 4, 2, 8, 1, 7, 7};
+    static constexpr const auto m{8};
+    const vector<int32_t> result{7, 7, 5, 4, 3, 4, 4, 0};
+    CHECK_THAT(result, Equals(leetcode_1409::processQueries(input, m)));
+    CHECK_THAT(result, Equals(leetcode_1409::processQueries2(input, m)));
+}
 }
 #endif //CS203_DSAA_TEMPLATE_ALGORITHM_ASSOCIATIVE_CONTAINER_LEETCODE_1409_TEST_HPP
