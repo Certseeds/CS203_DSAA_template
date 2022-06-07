1896cc00e82b5b3326e838d4b005845b4a7b8cf3
Certseeds <51754303+Certseeds@users.noreply.github.com>
2022-06-07 12:15:01 +0000

    feature: update lc-942
    </subject>
    
    Branch: dev
    
    <type>:
    - [ ] Bug fix
    - [ ] Bug fix (Test)
    - [x] New feature
    - [ ] Breaking change
    - [ ] Documentation update
    - [ ] This change requires a documentation update
    
    <body>
    
    <footer>
    
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

---
+++ algorithm/array/leetcode_942.cpp
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+/*
+CS203_DSAA_template
+
+Copyright (C) 2022  nanoseeds
+
+*/
+#include "leetcode_942_test.hpp"
+
+namespace leetcode_942 {
+
+vector<int32_t> leetcode_942::diStringMatch(const string &str) {
+    const auto str_size{str.size()};
+    int32_t min{0}, max{static_cast<int32_t>(str_size)};
+    vector<int32_t> willreturn(max + 1);
+    for (size_t i{0}; i < str_size; i++) {
+        if (str[i] == 'I') {
+            willreturn[i] = min;
+            min++;
+        } else {
+            willreturn[i] = max;
+            max--;
+        }
+    }
+    willreturn.back() = min;
+    return willreturn;
+}
+
+}
