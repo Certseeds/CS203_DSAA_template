520b3299f719ee299fd180a571581870c53b904d
Certseeds <51754303+Certseeds@users.noreply.github.com>
2022-06-11 13:16:46 +0000

    feature: update lc-944
    </subject>
    
    Branch: dev
    
    <type>:
    - [ ] Bug fix
    - [ ] Bug fix (Test)
    - [x] New feature
    - [ ] Breaking change
    - [ ] Documentation update
    - [ ] This change requires a documentation update
    
    <body>
    
    <footer>
    
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

---
+++ algorithm/array/leetcode_944.cpp
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+/*
+CS203_DSAA_template
+
+Copyright (C) 2022  nanoseeds
+
+*/
+#include "leetcode_944_test.hpp"
+
+namespace leetcode_944 {
+
+int leetcode_944::minDeletionSize(const vector<string> &strs) {
+    int count{0};
+    const auto lines{strs.size()};
+    const auto str_size{strs[0].size()};
+    for (size_t i{0},notdeletex{true}; i < str_size; ++i,notdeletex = true) {
+        for (size_t j{1}; j < lines; ++j) {
+            notdeletex = notdeletex & (strs[j][i] >= strs[j - 1][i]);
+        }
+        count += !notdeletex;
+    }
+    return count;
+}
+
+}
---
+++ algorithm/array/leetcode_944_test.hpp
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+/*
+CS203_DSAA_template
+
+Copyright (C) 2022  nanoseeds
+
+*/
+//@Tag array
+//@Tag 
+#ifndef CS203_DSAA_TEMPLATE_ALGORITHM_ARRAY_LEETCODE_944_TEST_HPP
+#define CS203_DSAA_TEMPLATE_ALGORITHM_ARRAY_LEETCODE_944_TEST_HPP
+
+#include <catch_main.hpp>
+#include <cstdint>
+#include <cstddef>
+#include <vector>
+#include <string>
+
+namespace leetcode_944 {
+using std::vector;
+using std::string;
+
+struct leetcode_944 {
+    static int minDeletionSize(const vector<string> &A);
+};
+
+TEST_CASE("test case 1 {test_944}", "{test_944}") {
+    static constexpr const std::array<const char *const, 3> input{
+            "abc", "bce", "cae"
+    };
+    static constexpr const auto result{1};
+    CHECK(result == leetcode_944::minDeletionSize({input.begin(), input.end()}));
+}
+
+TEST_CASE("test case 2 {test_944}", "{test_944}") {
+    static constexpr const std::array<const char *const, 2> input{
+            "a", "b"
+    };
+    static constexpr const auto result{0};
+    CHECK(result  == leetcode_944::minDeletionSize({input.begin(), input.end()}));
+}
+TEST_CASE("test case 3 {test_944}", "{test_944}") {
+    static constexpr const std::array<const char *const, 3> input{
+            "zyx", "wvu", "tsr"
+    };
+    static constexpr const auto result{3};
+    CHECK(result == leetcode_944::minDeletionSize({input.begin(), input.end()}));
+}
+}
+#endif //CS203_DSAA_TEMPLATE_ALGORITHM_ARRAY_LEETCODE_944_TEST_HPP
