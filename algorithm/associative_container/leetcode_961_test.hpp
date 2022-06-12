22ad186e013140cbe1c68eeca4541c3ea718d792
Certseeds <51754303+Certseeds@users.noreply.github.com>
2022-06-12 02:49:35 +0000

    feature: update lc-961
    </subject>
    
    Branch: dev
    
    <type>:
    - [ ] Bug fix
    - [ ] Bug fix (Test)
    - [x] New feature
    - [ ] Breaking change
    - [ ] Documentation update
    - [ ] This change requires a documentation update
    
    <body>
    
    <footer>
    
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

---
+++ algorithm/associative_container/leetcode_961_test.hpp
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+/*
+CS203_DSAA_template
+
+Copyright (C) 2022  nanoseeds
+
+*/
+//@Tag associative_container
+//@Tag xor
+#ifndef CS203_DSAA_TEMPLATE_ALGORITHM_ASSOCIATIVE_CONTAINER_LEETCODE_961_TEST_HPP
+#define CS203_DSAA_TEMPLATE_ALGORITHM_ASSOCIATIVE_CONTAINER_LEETCODE_961_TEST_HPP
+
+#include <catch_main.hpp>
+#include <cstdint>
+#include <cstddef>
+#include <vector>
+#include <array>
+
+namespace leetcode_961 {
+using std::array;
+using std::vector;
+
+struct leetcode_961 {
+    static int32_t repeatedNTimes(const vector<int32_t> &nums);
+};
+
+TEST_CASE("test case 1 {test_961}", "{test_961}") {
+    static constexpr const std::array<int32_t, 4> arr{
+            1, 2, 3, 3
+    };
+    static constexpr const auto result{3};
+    CHECK(result == leetcode_961::repeatedNTimes({arr.cbegin(), arr.cend()}));
+}
+
+TEST_CASE("test case 2 {test_961}", "{test_961}") {
+    static constexpr const std::array<int32_t, 10> arr{
+            1, 0, 4, 4, 4, 4, 4, 5, 1, 4
+    };
+    static constexpr const auto result{4};
+    CHECK(result == leetcode_961::repeatedNTimes({arr.cbegin(), arr.cend()}));
+}
+
+TEST_CASE("test case 3 {test_961}", "{test_961}") {
+    static constexpr const std::array<int32_t, 10> arr{
+            1, 9, 2, 9, 8, 9, 3, 9, 0, 9
+    };
+    static constexpr const auto result{9};
+    CHECK(result == leetcode_961::repeatedNTimes({arr.cbegin(), arr.cend()}));
+}
+}
+#endif //CS203_DSAA_TEMPLATE_ALGORITHM_ASSOCIATIVE_CONTAINER_LEETCODE_961_TEST_HPP
