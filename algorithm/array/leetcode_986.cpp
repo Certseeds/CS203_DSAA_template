53288c8d12fe71f976dc411c06869bda8b93eea4
Certseeds <51754303+Certseeds@users.noreply.github.com>
2022-06-28 13:24:49 +0000

    feature: update lc-986
    </subject>
    
    Branch: dev
    
    <type>:
    - [ ] Bug fix
    - [ ] Bug fix (Test)
    - [x] New feature
    - [ ] Breaking change
    - [ ] Documentation update
    - [ ] This change requires a documentation update
    
    <body>
    
    <footer>
    
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

---
+++ algorithm/array/leetcode_986.cpp
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+/*
+CS203_DSAA_template
+
+Copyright (C) 2022  nanoseeds
+
+*/
+#include "leetcode_986_test.hpp"
+
+namespace leetcode_986 {
+
+vector<vector<int>> leetcode_986::intervalIntersection(const vector<vector<int>> &fst, const vector<vector<int>> &snd) {
+    vector<vector<int32_t>> will_return;
+    const auto fst_size{fst.size()}, snd_size{snd.size()};
+    const auto judgement = [&fst, &snd](size_t pre, size_t post) -> vector<int32_t> {
+        const auto &fst_element{fst[pre]}, &snd_element{snd[post]};
+        return {std::max(fst_element[0], snd_element[0]),
+                std::min(fst_element[1], snd_element[1])};
+    };
+    for (size_t x{0}, y{0}, judge{0}; x < fst_size && y < snd_size;) {
+        if (fst[x][1] < snd[y][0]) {
+            ++x;
+        } else if (fst[x][0] > snd[y][1]) {
+            ++y;
+        } else {
+            const vector<int32_t> temp = judgement(x, y);
+            will_return.emplace_back(temp);
+            judge = ((temp[1] >= fst[x][1]) == 1 ? 1 : 0);
+            x += judge;
+            y += (1 - judge);
+        }
+    }
+    return will_return;
+}
+}
---
+++ algorithm/array/leetcode_986_test.hpp
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+/*
+CS203_DSAA_template
+
+Copyright (C) 2022  nanoseeds
+
+*/
+//@Tag array
+//@Tag 双指针
+#ifndef CS203_DSAA_TEMPLATE_ALGORITHM_ARRAY_LEETCODE_986_TEST_HPP
+#define CS203_DSAA_TEMPLATE_ALGORITHM_ARRAY_LEETCODE_986_TEST_HPP
+
+#include <catch_main.hpp>
+#include <cstdint>
+#include <cstddef>
+#include <vector>
+#include <string>
+
+namespace leetcode_986 {
+using std::vector;
+using std::string;
+
+struct leetcode_986 {
+    static vector<vector<int>> intervalIntersection(const vector<vector<int>> &fst, const vector<vector<int>> &snd);
+};
+
+
+using Catch::Matchers::Equals;
+
+TEST_CASE("test case 1 {test_986}", "{test_986}") {
+    const vector<vector<int32_t>> fst{{0,  2},
+                                      {5,  10},
+                                      {13, 23},
+                                      {24, 25}};
+    const vector<vector<int32_t>> snd{{1,  5},
+                                      {8,  12},
+                                      {15, 24},
+                                      {25, 26}};
+    const vector<vector<int32_t>> result{{1,  2},
+                                         {5,  5},
+                                         {8,  10},
+                                         {15, 23},
+                                         {24, 24},
+                                         {25, 25}};
+    CHECK_THAT(result, Equals(leetcode_986::intervalIntersection(fst, snd)));
+}
+
+TEST_CASE("test case 3 {test_986}", "{test_986}") {
+    const vector<vector<int32_t>> fst{{17, 20}};
+    const vector<vector<int32_t>> snd{{2,  3},
+                                      {6,  8},
+                                      {12, 14},
+                                      {19, 20}};
+    const vector<vector<int32_t>> result{{19, 20}};
+    CHECK_THAT(result, Equals(leetcode_986::intervalIntersection(fst, snd)));
+}
+
+}
+#endif //CS203_DSAA_TEMPLATE_ALGORITHM_ARRAY_LEETCODE_986_TEST_HPP
