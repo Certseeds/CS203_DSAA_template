a7f55d781c245b80926c3353eb3e4a717b232054
Certseeds <51754303+Certseeds@users.noreply.github.com>
2022-07-08 13:59:57 +0000

    feature: update lc-1013
    </subject>
    
    Branch: dev
    
    <type>:
    - [ ] Bug fix
    - [ ] Bug fix (Test)
    - [x] New feature
    - [ ] Breaking change
    - [ ] Documentation update
    - [ ] This change requires a documentation update
    
    <body>
    
    <footer>
    
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

---
+++ algorithm/array/leetcode_1013_test.hpp
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+/*
+CS203_DSAA_template
+
+Copyright (C) 2022  nanoseeds
+
+*/
+//@Tag array
+//@Tag 数组
+//@Tag "双"指针
+#ifndef CS203_DSAA_TEMPLATE_ALGORITHM_ARRAY_LEETCODE_1013_TEST_HPP
+#define CS203_DSAA_TEMPLATE_ALGORITHM_ARRAY_LEETCODE_1013_TEST_HPP
+
+#include <catch_main.hpp>
+#include <cstdint>
+#include <cstddef>
+#include <vector>
+
+namespace leetcode_1013 {
+using std::vector;
+
+struct leetcode_1013 {
+    static bool canThreePartsEqualSum(const vector<int32_t> &arr);
+};
+
+TEST_CASE("test case 1 {test_1013}", "{test_1013}") {
+    const vector<int32_t> input{1, -1, 1, -1};
+    CHECK_FALSE(leetcode_1013::canThreePartsEqualSum(input));
+}
+
+TEST_CASE("test case 5 {test_1013}", "{test_1013}") {
+    const vector<int32_t> input{0, 1, -1, 1, -1, 0};
+    CHECK(leetcode_1013::canThreePartsEqualSum(input));
+}
+
+TEST_CASE("test case 2 {test_1013}", "{test_1013}") {
+    const vector<int32_t> input{3, 3, 6, 5, -2, 2, 5, 1, -9, 4};
+    CHECK(leetcode_1013::canThreePartsEqualSum(input));
+}
+
+TEST_CASE("test case 3 {test_1013}", "{test_1013}") {
+    const vector<int32_t> input{0, 2, 1, -6, 6, -7, 9, 1, 2, 0, 1};
+    CHECK(leetcode_1013::canThreePartsEqualSum(input));
+}
+
+TEST_CASE("test case 4 {test_1013}", "{test_1013}") {
+    const vector<int32_t> input{0, 2, 1, -6, 6, 7, 9, -1, 2, 0, 1};
+    CHECK_FALSE(leetcode_1013::canThreePartsEqualSum(input));
+}
+}
+#endif //CS203_DSAA_TEMPLATE_ALGORITHM_ARRAY_LEETCODE_1013_TEST_HPP
