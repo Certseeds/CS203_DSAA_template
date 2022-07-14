77ba4b894ff60dc90c2be401fda1f7169584c7c6
Certseeds <51754303+Certseeds@users.noreply.github.com>
2022-07-14 14:01:15 +0000

    feature: update lc-1021
    </subject>
    
    Branch: dev
    
    <type>:
    - [ ] Bug fix
    - [ ] Bug fix (Test)
    - [x] New feature
    - [ ] Breaking change
    - [ ] Documentation update
    - [ ] This change requires a documentation update
    
    <body>
    
    <footer>
    
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

---
+++ algorithm/stack/leetcode_1021_test.hpp
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+/*
+CS203_DSAA_template
+
+Copyright (C) 2020-2022 nanoseeds
+
+*/
+#ifndef CS203_DSAA_TEMPLATE_ALGORITHM_STACK_LEETCODE_1021_TEST_HPP
+#define CS203_DSAA_TEMPLATE_ALGORITHM_STACK_LEETCODE_1021_TEST_HPP
+
+#include <catch_main.hpp>
+#include <string>
+
+namespace leetcode_1021 {
+using std::string;
+
+struct leetcode_1021 {
+    static string removeOuterParentheses(const string &str);
+};
+
+TEST_CASE("1 [test_1021]", "[test_1021]") {
+    static constexpr const char *const input{"(()())(())"};
+    static constexpr const char *const result{"()()()"};
+    CHECK(result == leetcode_1021::removeOuterParentheses(input));
+}
+
+TEST_CASE("2 [test_1021]", "[test_1021]") {
+    static constexpr const char *const input{"(())(()()())((()))"};
+    static constexpr const char *const result{"()()()()(())"};
+    CHECK(result == leetcode_1021::removeOuterParentheses(input));
+}
+
+TEST_CASE("3 [test_1021]", "[test_1021]") {
+    static constexpr const char *const input{"()()"};
+    static constexpr const char *const result{""};
+    CHECK(result == leetcode_1021::removeOuterParentheses(input));
+}
+}
+#endif //CS203_DSAA_TEMPLATE_ALGORITHM_STACK_LEETCODE_1021_TEST_HPP
