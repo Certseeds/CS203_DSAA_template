b08c4adead42af187c56a1608fdf4afc368aa3e7
Certseeds <51754303+Certseeds@users.noreply.github.com>
2022-07-16 12:31:11 +0000

    feature: update lc-1022
    </subject>
    
    Branch: dev
    
    <type>:
    - [ ] Bug fix
    - [ ] Bug fix (Test)
    - [x] New feature
    - [ ] Breaking change
    - [ ] Documentation update
    - [ ] This change requires a documentation update
    
    <body>
    
    <footer>
    
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

---
+++ algorithm/tree/leetcode_1022_test.hpp
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+/*
+CS203_DSAA_template
+
+Copyright (C) 2022  nanoseeds
+
+*/
+//@Tag tree
+//@Tag 树
+#ifndef CS203_DSAA_TEMPLATE_ALGORITHM_TREE_LEETCODE_1022_TEST_HPP
+#define CS203_DSAA_TEMPLATE_ALGORITHM_TREE_LEETCODE_1022_TEST_HPP
+
+#include <catch_main.hpp>
+#include <cstdint>
+#include <cstddef>
+#include <tree/treenode.hpp>
+#include <tree/treenode_link.hpp>
+
+namespace leetcode_1022 {
+using TreeNode = TREE_NODE::TreeNode<int32_t>;
+
+struct leetcode_1022 final {
+    static int32_t sumRootToLeaf(TreeNode *root);
+};
+
+
+using TreeNodeLink = TREE_NODE::TreeNodeLink<int32_t>;
+
+TEST_CASE("test_case 2 [test_1022]", "[test_1022]") {
+    const TreeNodeLink input{
+            1,
+            0, 1,
+            0, 1, 0, 1,
+            TreeNode::No, TreeNode::No, TreeNode::No, TreeNode::No,
+            TreeNode::No, TreeNode::No, TreeNode::No, TreeNode::No
+    };
+    static constexpr const auto result{22};
+    CHECK(result == leetcode_1022::sumRootToLeaf(input[0]));
+}
+
+TEST_CASE("test_case 1 [test_1022]", "[test_1022]") {
+    const TreeNodeLink input{
+            0,
+            TreeNode::No, TreeNode::No
+    };
+    static constexpr const auto result{0};
+    CHECK(result == leetcode_1022::sumRootToLeaf(input[0]));
+}
+
+}
+#endif //CS203_DSAA_TEMPLATE_ALGORITHM_TREE_LEETCODE_1022_TEST_HPP
