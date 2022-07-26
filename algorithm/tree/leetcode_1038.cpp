76dfb77a289d9d1ce857c5b8c69736d94bd12acd
Certseeds <51754303+Certseeds@users.noreply.github.com>
2022-07-26 13:19:26 +0000

    feature: update lc-1038
    </subject>
    
    Branch: dev
    
    <type>:
    - [ ] Bug fix
    - [ ] Bug fix (Test)
    - [x] New feature
    - [ ] Breaking change
    - [ ] Documentation update
    - [ ] This change requires a documentation update
    
    <body>
    
    <footer>
    
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

---
+++ algorithm/tree/leetcode_1038.cpp
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+/*
+CS203_DSAA_template
+
+Copyright (C) 2022 nanoseeds
+
+*/
+#include "leetcode_1038_test.hpp"
+
+namespace leetcode_1038 {
+
+int32_t rec(TreeNode *root, int32_t sum) { // 最小和数大概是反过来?
+    if (root == nullptr) {
+        return sum;
+    } else if (root->left == nullptr && root->right == nullptr) {
+        root->val += sum;
+        return root->val;
+    }
+    const auto rightMax{rec(root->right, sum)};
+    root->val += rightMax;
+    const auto leftMax{rec(root->left, root->val)};
+    return leftMax;
+}
+
+TreeNode *leetcode_1038::bstToGst(TreeNode *root) {
+    rec(root, 0);
+    return root;
+}
+
+}
---
+++ algorithm/tree/leetcode_1038_test.hpp
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+/*
+CS203_DSAA_template
+
+Copyright (C) 2022  nanoseeds
+
+*/
+//@Tag tree
+//@Tag 树
+//@Tag 更大和数
+#ifndef CS203_DSAA_TEMPLATE_ALGORITHM_TREE_LEETCODE_1038_TEST_HPP
+#define CS203_DSAA_TEMPLATE_ALGORITHM_TREE_LEETCODE_1038_TEST_HPP
+
+#include <catch_main.hpp>
+#include <cstdint>
+#include <cstddef>
+#include <tree/treenode.hpp>
+#include <tree/treenode_link.hpp>
+
+namespace leetcode_1038 {
+using TreeNode = TREE_NODE::TreeNode<int32_t>;
+
+struct leetcode_1038 final {
+    static TreeNode *bstToGst(TreeNode *root);
+};
+
+using TreeNodeLink = TREE_NODE::TreeNodeLink<int32_t>;
+
+TEST_CASE("test_case 1 [test_1038]", "[test_1038]") {
+    const TreeNodeLink input{
+            4,
+            1, 6,
+            0, 2, 5, 7,
+            TreeNode::No, TreeNode::No, TreeNode::No, 3, TreeNode::No, TreeNode::No, TreeNode::No, 8,
+            TreeNode::No, TreeNode::No, TreeNode::No, TreeNode::No
+    };
+    const vector<int32_t> result{
+            30,
+            36, 21,
+            36, 35, 26, 15,
+            TreeNode::No, TreeNode::No, TreeNode::No, 33, TreeNode::No, TreeNode::No, TreeNode::No, 8,
+            TreeNode::No, TreeNode::No, TreeNode::No, TreeNode::No
+    };
+    const auto output = leetcode_1038::bstToGst(input[0]);
+    CHECK(TREE_NODE::judge_equal(output, result));
+}
+
+TEST_CASE("test_case 2 [test_1038]", "[test_1038]") {
+    const TreeNodeLink input{
+            0,
+            TreeNode::No, 1, TreeNode::No, TreeNode::No
+    };
+    const vector<int32_t> result{
+            1,
+            TreeNode::No, 1, TreeNode::No, TreeNode::No
+    };
+    const auto output = leetcode_1038::bstToGst(input[0]);
+    CHECK(TREE_NODE::judge_equal(output, result));
+}
+}
+#endif //CS203_DSAA_TEMPLATE_ALGORITHM_TREE_LEETCODE_1038_TEST_HPP
