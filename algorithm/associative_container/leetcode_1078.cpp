a383c8dece47f06ac4289a1865f3211542bdc4ac
Certseeds <51754303+Certseeds@users.noreply.github.com>
2022-08-01 14:18:01 +0000

    feature: update lc-1078
    </subject>
    
    Branch: dev
    
    <type>:
    - [ ] Bug fix
    - [ ] Bug fix (Test)
    - [x] New feature
    - [ ] Breaking change
    - [ ] Documentation update
    - [ ] This change requires a documentation update
    
    <body>
    
    <footer>
    
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

---
+++ algorithm/associative_container/leetcode_1078.cpp
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+/*
+CS203_DSAA_template
+
+Copyright (C) 2022  nanoseeds
+
+*/
+#include "leetcode_1078_test.hpp"
+
+namespace leetcode_1078 {
+
+vector<string> leetcode_1078::findOcurrences(const string &text, const string &first, const string &second) {
+    vector<unsigned long> vecs;
+    vector<string> words{}, results{};
+    const auto fstHash = std::hash<string>()(first);
+    const auto sndHash = std::hash<string>()(second);
+    string texts_{text};
+    texts_.push_back(' ');
+    const string &texts{texts_};
+    for (size_t i{0}, length{1}; i < texts.size();) {
+        if (texts[i + length] == ' ') {
+            const auto substr{texts.substr(i, length)};
+            vecs.push_back(std::hash<string>()(substr));
+            words.push_back(substr);
+            i += length + 1;
+            length = 1;
+        } else {
+            length += 1;
+        }
+    }
+    for (size_t i{1}; i + 1 < vecs.size(); ++i) {
+        if (vecs[i - 1] == fstHash && vecs[i] == sndHash) {
+            results.push_back(words[i + 1]);
+        }
+    }
+    return results;
+}
+
+}
---
+++ algorithm/associative_container/leetcode_1078_test.hpp
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+/*
+CS203_DSAA_template
+
+Copyright (C) 2022  nanoseeds
+
+*/
+//@Tag associative_container
+
+#ifndef CS203_DSAA_TEMPLATE_ALGORITHM_ASSOCIATIVE_CONTAINER_LEETCODE_1078_TEST_HPP
+#define CS203_DSAA_TEMPLATE_ALGORITHM_ASSOCIATIVE_CONTAINER_LEETCODE_1078_TEST_HPP
+
+#include <catch_main.hpp>
+#include <cstdint>
+#include <cstddef>
+#include <vector>
+#include <array>
+
+namespace leetcode_1078 {
+using std::vector;
+
+struct leetcode_1078 {
+    static vector<string> findOcurrences(const string &text, const string &first, const string &second);
+};
+
+using Catch::Matchers::Equals;
+
+TEST_CASE("test case 1 {test_1078}", "{test_1078}") {
+    static constexpr const char *const text{"how are you indian mi fans do you like mi four i"};
+    static constexpr const char *const fst{"mi"};
+    static constexpr const char *const snd{"fans"};
+    const vector<string> result{"do"};
+    CHECK_THAT(result, Equals(leetcode_1078::findOcurrences(text, fst, snd)));
+}
+}
+#endif //CS203_DSAA_TEMPLATE_ALGORITHM_ASSOCIATIVE_CONTAINER_LEETCODE_1078_TEST_HPP
