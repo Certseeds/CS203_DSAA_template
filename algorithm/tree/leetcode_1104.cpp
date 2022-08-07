cc015b455b9543d81913aa6a5b1c1f16745340c3
Certseeds <51754303+Certseeds@users.noreply.github.com>
2022-08-07 13:09:03 +0000

    feature: update lc-1104
    </subject>
    
    Branch: dev
    
    <type>:
    - [ ] Bug fix
    - [ ] Bug fix (Test)
    - [x] New feature
    - [ ] Breaking change
    - [ ] Documentation update
    - [ ] This change requires a documentation update
    
    <body>
    
    <footer>
    
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

---
+++ algorithm/tree/leetcode_1104.cpp
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+/*
+CS203_DSAA_template
+
+Copyright (C) 2022 nanoseeds
+
+*/
+#include "leetcode_1104_test.hpp"
+
+namespace leetcode_1104 {
+
+vector<int32_t> leetcode_1104::pathInZigZagTree(int32_t label) {
+    // 第一步,先把在哪一行计算出来
+    const auto input{label};
+    const auto [level, number] = [input]() {
+        int32_t lev{0}, num{0};
+        for (; num < input; ++lev) {
+            num += (1 << lev);
+        }
+        return std::make_tuple(lev, num);
+    }();
+    // 拿到最后一行从0开始,从左向右数的序数
+    const auto final_order = [](auto num_diff, auto lev) {
+        if (lev % 2 == 0) {
+            return (1 << lev) - num_diff - 1;
+        } else {
+            return num_diff;
+        }
+    }(number - input, level - 1);
+    // 得到每一行自0开始,从左向右的序数(反向排列,一会儿需要用crbegin())
+    vector<int32_t> each_level{};
+    for (auto order_num{final_order}, lev{0}; lev < level; order_num /= 2, ++lev) {
+        each_level.push_back(order_num);
+    }
+    // 一行一行的对应到数字
+    vector<int32_t> will_return{};
+    auto iter = each_level.crbegin();
+    for (int32_t lev{0}, min{1}; lev < level; ++lev, ++iter) {
+        if (lev % 2 == 0) {
+            will_return.push_back(min + *iter);
+        } else {
+            will_return.push_back(min - *iter + (1 << lev) - 1);
+        }
+        min += (1 << lev);
+    }
+    return will_return;
+}
+
+}
