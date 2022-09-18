148570cdb45e2e67c64b19b9412d33e6a74ea973
Certseeds <51754303+Certseeds@users.noreply.github.com>
2022-09-18 04:40:33 +0000

    feature: add lc-1200
    </subject>
    
    Branch: dev
    
    <type>:
    - [ ] Bug fix
    - [ ] Bug fix (Test)
    - [x] New feature
    - [ ] Breaking change
    - [ ] Documentation update
    - [ ] This change requires a documentation update
    
    <body>
    
    <footer>
    
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

---
+++ algorithm/array/leetcode_1200.cpp
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+/*
+CS203_DSAA_template
+
+Copyright (C) 2022  nanoseeds
+
+*/
+#include "leetcode_1200_test.hpp"
+
+namespace leetcode_1200 {
+
+vector<vector<int32_t>> leetcode_1200::minimumAbsDifference(const vector<int32_t> &arr_) {
+    vector<int32_t> arr{arr_};
+    std::sort(arr.begin(), arr.end());
+    int32_t minDiff = std::numeric_limits<int32_t>::max() - 1;
+    vector<vector<int32_t>> will_return{};
+    for (auto fst = arr.cbegin(), snd = fst + 1; snd != arr.cend(); ++fst, ++snd) {
+        const auto diff = (*snd - *fst);
+        if (minDiff > diff) {
+            minDiff = diff;
+            will_return.clear();
+            will_return.push_back({*fst, *snd});
+        } else if (minDiff == diff) {
+            will_return.push_back({*fst, *snd});
+        }
+    }
+    return will_return;
+}
+
+}
