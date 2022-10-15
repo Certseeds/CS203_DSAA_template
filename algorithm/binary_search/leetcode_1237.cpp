7a8d70b5f34945ec2bf4bb387c614974d1acc44c
Certseeds <51754303+Certseeds@users.noreply.github.com>
2022-10-15 09:08:40 +0000

    feature: update lc-1237
    </subject>
    
    Branch: dev
    
    <type>:
    - [ ] Bug fix
    - [ ] Bug fix (Test)
    - [x] New feature
    - [ ] Breaking change
    - [ ] Documentation update
    - [ ] This change requires a documentation update
    
    <body>
    
    <footer>
    
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

---
+++ algorithm/binary_search/leetcode_1237.cpp
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: MIT
+/*
+CS203_DSAA_template
+
+Copyright (C) 2022 nanos
+
+*/
+#include "leetcode_1237_test.hpp"
+
+#include <algorithm>
+
+namespace leetcode_1237 {
+int32_t getXMin(const CustomFunction &customfunction, int32_t z) {
+    // fst step, find the minimum x
+    // 如果f(1,1)就已经大于z了,就不需要进行了,直接退出
+    // 如果(50,1000)等于z, 说明(49,1000)小于z, 需要从50开始
+    // 如果(50,1000)大于z, 说明(49,1000)小于z, 需要从50开始
+    // 如果(50,1000)大于z, 说明(49,1000)等于z, 需要从49开始
+    const auto y{1000};
+    for (int32_t x_left{1}, x_right{1000}; x_left < x_right;) {
+        const auto mid = (x_right - x_left) / 2 + x_left;
+        const auto value{customfunction(mid, y)};
+        if (value == z) {
+            return mid;
+        } else if (value > z) {
+            const auto midM1{mid - 1};
+            const auto valueM1{customfunction(midM1, y)};
+            if (valueM1 < z) {
+                return mid;
+            }
+            x_right = mid;
+        } else {
+            x_left = mid + 1;
+        }
+    }
+    return 1;
+}
+
+int32_t getXMax(const CustomFunction &customfunction, int32_t z) {
+    // fst step, find the minimum x
+    // 如果f(1,1)就已经大于z了,就不需要进行了,直接退出
+    // 如果(50,1)等于z, 说明(51,1)大于z, 截止到50
+    // 如果(50,1)小于z, 说明(51,1)大于z, 截止到50
+    const auto y{1};
+    for (int32_t x_left{1}, x_right{1000}; x_left < x_right;) {
+        const auto mid = (x_right - x_left) / 2 + x_left;
+        const auto value{customfunction(mid, y)};
+        if (value == z) {
+            return mid;
+        } else if (value < z) {
+            const auto midP1 = mid + 1;
+            const auto valueP1{customfunction(midP1, y)};
+            if (valueP1 > z) {
+                return mid;
+            }
+            x_left = mid + 1;
+        } else {
+            x_right = mid;
+        }
+    }
+    return 1000;
+}
+
+vector<vector<int>> leetcode_1237::findSolution(const CustomFunction &customfunction, int32_t z) {
+    vector<vector<int32_t>> will_return{};
+    if (customfunction(1, 1) > z) {
+        return will_return;
+    } // O(1)守门
+    const auto xmin{getXMin(customfunction, z)}; // log(n)
+    const auto xmax{getXMax(customfunction, z)}; // log(n)
+    for (int32_t i{xmin}, y_righest{1000}; i <= xmax; ++i) { // log(n) for each step
+        // f(x1,y1) == z
+        // then, for f(x1+1,y2) == z, y2<y1
+        for (int32_t y_left{1}, y_right{y_righest}; y_left < y_right;) {
+            const auto mid = (y_right - y_left) / 2 + y_left;
+            const auto value = customfunction(i, mid);
+            if (value > z) {
+                y_right = mid;
+            } else if (value == z) {
+                will_return.emplace_back(vector<int32_t>{i, mid});
+                y_righest = y_right;
+                break;
+            } else {
+                y_left = mid + 1;
+            }
+        }
+    }
+    return will_return;
+}
+}
---
+++ algorithm/binary_search/leetcode_1237_test.hpp
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: MIT
+/*
+CS203_DSAA_template
+
+Copyright (C) 2022 nanos
+
+*/
+//@Tag array
+//@Tag 数组
+//@Tag binary search
+//@Tag 二分
+//@Tag 多重二分
+//@Level Mid
+#ifndef CS203_DSAA_TEMPLATE_ALGORITHM_BINARY_SEARCH_LEETCODE_TEST_1237_TEST_HPP
+#define CS203_DSAA_TEMPLATE_ALGORITHM_BINARY_SEARCH_LEETCODE_TEST_1237_TEST_HPP
+
+#include <catch_main.hpp>
+#include <cstdint>
+#include <cstddef>
+#include <vector>
+
+namespace leetcode_1237 {
+using std::vector;
+
+
+using CustomFunction = std::function<int32_t(int32_t x, int32_t y)>;
+
+namespace leetcode_1237 {
+vector<vector<int>> findSolution(const CustomFunction &customfunction, int32_t z);
+}
+
+using Catch::Matchers::Equals;
+
+TEST_CASE("1 [test_1237]", "[test_1237]") {
+    const auto func = [](int32_t x, int32_t y) {
+        return x + y;
+    };
+    static constexpr const auto z{5};
+    const auto result = leetcode_1237::findSolution(func, z);
+    const vector<vector<int32_t>> output{{1, 4},
+                                         {2, 3},
+                                         {3, 2},
+                                         {4, 1}};
+    CHECK_THAT(output, Equals(result));
+}
+
+TEST_CASE("2 [test_1237]", "[test_1237]") {
+    const auto func = [](int32_t x, int32_t y) {
+        return x * y;
+    };
+    static constexpr const auto z{5};
+    const auto result = leetcode_1237::findSolution(func, z);
+    const vector<vector<int32_t>> output{{1, 5}, // it's not the age of traditional addition
+                                         {5, 1}};
+    CHECK_THAT(output, Equals(result));
+}
+
+}
+
+#endif //CS203_DSAA_TEMPLATE_ALGORITHM_BINARY_SEARCH_LEETCODE_TEST_1237_TEST_HPP
