25ce1cc684e804cb997e74ad2d9c9892baf36c49
Certseeds <51754303+Certseeds@users.noreply.github.com>
2022-10-22 12:12:54 +0000

    feature: done lc-1261
    </subject>
    
    Branch: dev
    
    <type>:
    - [ ] Bug fix
    - [ ] Bug fix (Test)
    - [x] New feature
    - [ ] Breaking change
    - [ ] Documentation update
    - [ ] This change requires a documentation update
    
    <body>
    
    <footer>
    
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

---
+++ algorithm/tree/leetcode_1261.cpp
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: MIT
+/*
+CS203_DSAA_template
+
+Copyright (C) 2022 nanoseeds
+
+*/
+#include "leetcode_1261_test.hpp"
+
+#include <stack>
+
+namespace leetcode_1261 {
+class tree : public findElements {
+private:
+    TreeNode *const root;
+public:
+    explicit tree(TreeNode *root_) : root(root_) {
+        TreeNode *node = root;
+        node->val = 0;
+        for (std::stack<TreeNode *> sta{{node}}; !sta.empty();) {
+            TreeNode *const top = sta.top();
+            sta.pop();
+            if (top != nullptr) {
+                if (top->left != nullptr) {
+                    top->left->val = (top->val * 2) + 1;
+                    sta.push(top->left);
+                }
+                if (top->right != nullptr) {
+                    top->right->val = (top->val * 2) + 2;
+                    sta.push(top->right);
+                }
+            }
+        }
+    }
+
+    [[nodiscard]] bool find(int32_t target) const override {
+        std::stack<uint8_t> sta;
+        for (int32_t temp{target}; temp != 0;) {
+            if (temp % 2 == 0) {
+                sta.push(1); // means right
+                temp = (temp - 2) / 2;
+            } else {
+                sta.push(0);
+                temp = (temp - 1) / 2;
+            }
+        }
+        // 15: (1,7),(1,3),(1,1),(1,0)
+        // 30: (0,14),(0,6),(0,2),(0,0)
+        const TreeNode *base{root};
+        while (!sta.empty()) {
+            const auto v = sta.top();
+            sta.pop();
+            if (base == nullptr) {
+                return false;
+            }
+            if (v == 1) {
+                base = base->right;
+            } else {
+                base = base->left;
+            }
+        }
+        return (base != nullptr);
+    }
+};
+
+findElements *init(TreeNode *root) {
+    return new tree(root);
+}
+
+}
---
+++ algorithm/tree/leetcode_1261_test.hpp
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: MIT
+/*
+CS203_DSAA_template
+
+Copyright (C) 2022  nanoseeds
+
+*/
+//@Tag tree
+//@Tag 树
+//@Tag 更大和数
+#ifndef CS203_DSAA_TEMPLATE_ALGORITHM_TREE_LEETCODE_1261_TEST_HPP
+#define CS203_DSAA_TEMPLATE_ALGORITHM_TREE_LEETCODE_1261_TEST_HPP
+
+#include <catch_main.hpp>
+#include <cstdint>
+#include <cstddef>
+#include <tree/treenode.hpp>
+#include <tree/treenode_link.hpp>
+
+namespace leetcode_1261 {
+using TreeNode = TREE_NODE::TreeNode<int32_t>;
+
+constexpr const auto junk{-1};
+
+class findElements {
+public:
+    [[nodiscard]] virtual bool find(int target) const = 0;
+};
+
+findElements *init(TreeNode *root);
+
+using TreeNodeLink = TREE_NODE::TreeNodeLink<int32_t>;
+using Catch::Matchers::Equals;
+
+TEST_CASE("test_case 1 [test_1261]", "[test_1261]") {
+    const TreeNodeLink input{
+            junk,
+            TreeNode::No, junk,
+            TreeNode::No, TreeNode::No, TreeNode::No, TreeNode::No,
+    };
+    const auto *const tree = init(input[0]);
+    CHECK(tree->find(0));
+    CHECK_FALSE(tree->find(1));
+    CHECK(tree->find(2));
+    CHECK_FALSE(tree->find(3));
+    CHECK_FALSE(tree->find(4));
+}
+
+TEST_CASE("test_case 2 [test_1261]", "[test_1261]") {
+    const TreeNodeLink input{
+            junk,
+            junk, junk,
+            junk, junk, TreeNode::No, TreeNode::No,
+            TreeNode::No, TreeNode::No, TreeNode::No, TreeNode::No, TreeNode::No, TreeNode::No, TreeNode::No,
+            TreeNode::No,
+    };
+    const auto *const tree = init(input[0]);
+    CHECK(tree->find(0));
+    CHECK(tree->find(1));
+    CHECK(tree->find(2));
+    CHECK(tree->find(3));
+    CHECK(tree->find(4));
+    CHECK_FALSE(tree->find(5));
+    CHECK_FALSE(tree->find(6));
+    CHECK_FALSE(tree->find(7));
+}
+
+}
+#endif //CS203_DSAA_TEMPLATE_ALGORITHM_TREE_LEETCODE_1261_TEST_HPP
