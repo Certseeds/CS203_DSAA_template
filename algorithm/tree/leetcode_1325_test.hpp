2ed8f600dd1f41d6792c820160e85280ad34808d
Certseeds <51754303+Certseeds@users.noreply.github.com>
2022-11-19 12:38:39 +0000

    feature: update lc-1325
    </subject>
    
    Branch: dev
    
    <type>:
    - [ ] Bug fix
    - [ ] Bug fix (Test)
    - [x] New feature
    - [ ] Breaking change
    - [ ] Documentation update
    - [ ] This change requires a documentation update
    
    <body>
    
    <footer>
    
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

---
+++ algorithm/tree/leetcode_1325_test.hpp
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+/*
+CS203_DSAA_template
+
+Copyright (C) 2022  nanoseeds
+
+*/
+//@Tag tree
+//@Tag 树
+#ifndef CS203_DSAA_TEMPLATE_ALGORITHM_TREE_LEETCODE_1325_TEST_HPP
+#define CS203_DSAA_TEMPLATE_ALGORITHM_TREE_LEETCODE_1325_TEST_HPP
+
+#include <catch_main.hpp>
+#include <cstdint>
+#include <cstddef>
+#include <tree/treenode.hpp>
+#include <tree/treenode_link.hpp>
+
+namespace leetcode_1325 {
+using TreeNode = TREE_NODE::TreeNode<int32_t>;
+
+namespace leetcode_1325 {
+TreeNode *removeLeafNodesRec(TreeNode *root, int32_t target);
+
+
+TreeNode *removeLeafNodesIter(TreeNode *root, int32_t target);
+}
+
+using TreeNodeLink = TREE_NODE::TreeNodeLink<int32_t>;
+using Catch::Matchers::Equals;
+
+TEST_CASE("test_case 1 [test_1325]", "[test_1325]") {
+    const TreeNodeLink input{
+            1,
+            1, 4,
+            5, 1, TreeNode::No, 4,
+            TreeNode::No, TreeNode::No, TreeNode::No, TreeNode::No, TreeNode::No, TreeNode::No, 1, 9,
+    };
+    const auto root{input[0]};
+    constexpr const auto target{1};
+    const auto outputRec = leetcode_1325::removeLeafNodesRec(root, target);
+    const vector<int32_t> result{
+            1,
+            1, 4,
+            5, TreeNode::No, TreeNode::No, 4,
+            TreeNode::No, TreeNode::No,/*TreeNode::No, TreeNode::No,TreeNode::No, TreeNode::No,*/ TreeNode::No, 9,
+            TreeNode::No, TreeNode::No,
+    };
+    CHECK(TREE_NODE::judge_equal(outputRec, result));
+}
+
+}
+#endif //CS203_DSAA_TEMPLATE_ALGORITHM_TREE_LEETCODE_1325_TEST_HPP
