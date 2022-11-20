6fd704cec2c0b45637e5de0146d7aac56d2b2beb
Certseeds <51754303+Certseeds@users.noreply.github.com>
2022-11-20 08:50:08 +0000

    feature: done lc-1325
    </subject>
    
    Branch: dev
    
    <type>:
    - [ ] Bug fix
    - [ ] Bug fix (Test)
    - [x] New feature
    - [ ] Breaking change
    - [ ] Documentation update
    - [ ] This change requires a documentation update
    
    <body>
    
    <footer>
    
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

---
+++ algorithm/string/leetcode_1328.cpp
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+/*
+CS203_DSAA_template
+
+Copyright (C) 2022 nanoseeds
+
+*/
+#include "leetcode_1328_test.hpp"
+
+namespace leetcode_1328 {
+
+string leetcode_1328::breakPalindrome(const string &palindrome) {
+    if (palindrome.size() == 1) {
+        return "";
+    }
+    string will_return{palindrome};
+    const auto length{palindrome.size()};
+    const auto mid{length / 2};
+    const auto addition = (length % 2 == 0 ? 0 : 1);
+    for (size_t iter{0}; iter < mid; ++iter) {
+        if (will_return[iter] > 'a') {
+            will_return[iter] = 'a';
+            return will_return;
+        }
+    }
+    for (size_t iter{mid + addition}; iter < length; ++iter) {
+        if (will_return[iter] > 'a') {
+            will_return[iter] = 'a';
+            return will_return;
+        }
+    }
+    for (size_t iter{length}; iter > mid + addition; --iter) {
+        if (will_return[iter - 1] == 'a') {
+            will_return[iter - 1] = 'b';
+            return will_return;
+        }
+    }
+    for (size_t iter{mid}; iter > 0; --iter) {
+        if (will_return[iter - 1] == 'a') {
+            will_return[iter - 1] = 'b';
+            return will_return;
+        }
+    }
+    // will_return[length - 1]='b';
+    //    return will_return;
+    return "";
+}
+}
---
+++ algorithm/string/leetcode_1328_test.hpp
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+/*
+CS203_DSAA_template
+
+Copyright (C) 2020-2022 nanoseeds
+
+*/
+//@Tag string
+//@Tag 字符串
+
+#ifndef CS203_DSAA_TEMPLATE_ALGORITHM_STRING_LEETCODE_1328_TEST_HPP
+#define CS203_DSAA_TEMPLATE_ALGORITHM_STRING_LEETCODE_1328_TEST_HPP
+
+#include <catch_main.hpp>
+#include <cstdint>
+#include <cstddef>
+#include <string>
+
+namespace leetcode_1328 {
+using std::string;
+
+namespace leetcode_1328 {
+string breakPalindrome(const string &palindrome);
+}
+
+TEST_CASE("1 [test_1328]", "[test_1328]") {
+    constexpr const char *const input{"abccba"};
+    constexpr const char *const result{"aaccba"};
+    const auto output = leetcode_1328::breakPalindrome(input);
+    CHECK(result == output);
+}
+
+TEST_CASE("2 [test_1328]", "[test_1328]") {
+    constexpr const char *const input{"aa"};
+    constexpr const char *const result{"ab"};
+    const auto output = leetcode_1328::breakPalindrome(input);
+    CHECK(result == output);
+}
+
+TEST_CASE("3 [test_1328]", "[test_1328]") {
+    constexpr const char *const input{"aba"};
+    constexpr const char *const result{"abb"};
+    const auto output = leetcode_1328::breakPalindrome(input);
+    CHECK(result == output);
+}
+
+TEST_CASE("4 [test_1328]", "[test_1328]") {
+    constexpr const char *const input{"aabaa"};
+    constexpr const char *const result{"aabab"};
+    const auto output = leetcode_1328::breakPalindrome(input);
+    CHECK(result == output);
+}
+
+}
+#endif //CS203_DSAA_TEMPLATE_ALGORITHM_STRING_LEETCODE_1328_TEST_HPP
