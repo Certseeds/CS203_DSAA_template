6cf10878d2ccb34e4aca62c98df9e814a990c58c
Certseeds <51754303+Certseeds@users.noreply.github.com>
2023-09-16 01:49:25 +0000

    release v0.11.1

---
+++ lab_01/lab_01_B/lab_01_B.cpp
@@ -0,0 +1,93 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+/**
+ * @Github: https://github.com/Certseeds/CS203_DSAA_template
+ * @Author: nanoseeds
+ * @Date: 2023-09-16 01:49:25
+ * @LastEditors: nanoseeds
+ */
+/*
+CS203_DSAA_template
+Copyright (C) 2020-2023 nanoseeds
+*/
+#include <list>
+#include <array>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <string>
+#include <vector>
+#include <numeric>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+
+#ifdef CS203_DSAA_TEST_MACRO
+namespace lab_01_B{
+#endif
+
+using std::cin;
+using std::tie;
+using std::cout;
+using std::list;
+using std::sort;
+using std::array;
+using std::deque;
+using std::queue;
+using std::stack;
+using std::tuple;
+using std::string;
+using std::vector;
+using std::unordered_map;
+using std::unordered_set;
+using std::priority_queue;
+static constexpr const char end{'\n'};
+
+using num_t = int32_t;
+using input_type = tuple<num_t, num_t>;
+using output_type = num_t;
+
+inline input_type read();
+
+output_type cal(input_type data);
+
+void output(const output_type &data);
+
+int main() {
+    auto input_data = read();
+    auto output_data = cal(input_data);
+    output(output_data);
+    return 0;
+}
+
+inline input_type read() {
+    num_t a{0},b{0};
+    std::cin >> a >> b;
+    return std::make_tuple(a, b);
+}
+
+output_type cal(input_type data) {
+    num_t a{0},b{0};
+    tie(a, b) = data;
+    num_t c = a + b;
+    return c;
+}
+
+void output(const output_type &data) {
+    cout << data << end;
+}
+static const auto faster_streams = [] {
+    srand(time(nullptr));
+    // use time to init the random seed
+    std::ios::sync_with_stdio(false);
+    std::istream::sync_with_stdio(false);
+    std::ostream::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    // 关闭c++风格输入输出 , 与C风格输入输出的同步,提高性能.
+    return 0;
+}();
+#ifdef CS203_DSAA_TEST_MACRO
+}
+#endif
---
+++ lab_01/lab_01_D/lab_01_D_test.cpp
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+/**
+ * @Github: https://github.com/Certseeds/CS203_DSAA_template
+ * @Author: nanoseeds
+ * @Date: 2023-09-16 01:49:25
+ * @LastEditors: nanoseeds
+ */
+/*
+CS203_DSAA_template
+Copyright (C) 2020-2023 nanoseeds
+*/
+#ifdef CS203_DSAA_TEST_MACRO
+
+#include <catch_main.hpp>
+#include <tuple>
+#include <vector>
+#include <iostream>
+
+#include "lab_01_D.cpp"
+
+std::string getFilePath() noexcept { return "./../../../lab_01/lab_01_D/resource/"; }
+const std::string CS203_redirect::file_paths = getFilePath();
+
+namespace lab_01_D {
+
+using std::tie;
+using std::cin;
+using std::cout;
+using std::tuple;
+using std::vector;
+
+using Catch::Matchers::Equals;
+using Catch::Matchers::UnorderedEquals;
+using Catch::Matchers::Contains;
+
+TEST_CASE("test case 1", "[test 01_D]") {
+    const auto output_data = cal(std::make_tuple(114, 514));
+    CHECK(output_data == 628);
+    CHECK(1 + 2 == 3);
+    vector <int32_t> vec{2, 7, 11, 15};
+    SECTION("CHECK_THAT 1") {
+        CHECK_THAT(vec, Contains<int>({2}));
+    }SECTION("vec matcher") {
+        CHECK_THAT(vec, UnorderedEquals<int>({15, 11, 7, 2}));
+    }
+}
+// CatchV3似乎不再支持[.]隐藏, 因此将sequence置为[1,0],隐藏输入
+TEST_CASE("test case with sequence", "[test 01_D]") {
+    CS203_sequence sequence{1, 0, 0}; // // 基础设定,[1,1]
+    sequence.set_postfix_of_datain("data.in"); // 输入数据后缀,默认为 data.in
+    sequence.set_postfix_of_dataout("data.out"); // except输出数据后缀,默认为 data.out
+    sequence.set_postfix_of_testout("test.out"); // 测试输出数据后缀,默认为 test.out
+    const auto files_name = sequence.get_files(true);
+    // 获取一个std::tuple<string,string,string> ,
+    // 其中每个tuple内为 `输入数据`,`except输出数据`,`测试输出数据`名.
+    for (const auto &file_name: files_name) {
+        string datain, dataout, testout; // 声明
+        tie(datain, dataout, testout) = file_name; // 解包
+        {
+            const CS203_redirect cr{datain, testout}; // 重定向输入,输出
+            main();
+            // 用括号括住是为了让CS203_redirect在这里被析构,停止重定向
+        }
+        CHECK(compareFiles(testout, dataout));
+    }
+}
+}
+#endif //CS203_DSAA_TEST_MACRO
